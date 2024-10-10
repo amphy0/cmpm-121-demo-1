1428b93abc9df2304fc330e205a6f50a946db136
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-10-10 11:59:12 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,31 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const sushiCounter = document.createElement("p")
+const sushiCounter = document.createElement("p");
 app.append(sushiCounter);
-
 
 const button = document.createElement("button");
 button.textContent = "Click Me 🍣";
 app.append(button);
 const setCounter = (count: number) => {
   counter = count;
-  sushiCounter.innerHTML = `number of 🍣: ${counter}`;
+  sushiCounter.innerHTML = `number of 🍣: ${counter.toFixed(0)}`;
 };
 button.addEventListener("click", () => setCounter(counter + 1));
-setInterval(() => setCounter(counter + 1), 1000);
+
+
+let lastTimestamp: number = performance.now();
+let deltaTime: number = 0;
+
+const animate = (timestamp: number) => {
+    deltaTime += (timestamp - lastTimestamp) / 1000;
+    lastTimestamp = timestamp;
+
+    if (deltaTime >= 1) {
+        setCounter(counter + deltaTime);
+        deltaTime = 0;
+    }
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);

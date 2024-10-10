7be28b926a05e1e736f8819896b58a57d3f54254
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-10-10 11:50:46 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,22 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 let counter: number = 0;
 
 const gameName = "My ok amazing game";
-document.title = gameName
+document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const sushiCounter = document.createElement("p")
+app.append(sushiCounter);
+
+
 const button = document.createElement("button");
 button.textContent = "Click Me 🍣";
 app.append(button);
 const setCounter = (count: number) => {
-    counter = count;
-    button.innerHTML = `number of 🍣: ${counter}`;
+  counter = count;
+  sushiCounter.innerHTML = `number of 🍣: ${counter}`;
 };
 button.addEventListener("click", () => setCounter(counter + 1));
+setInterval(() => setCounter(counter + 1), 1000);
